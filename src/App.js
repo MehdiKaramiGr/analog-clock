8ab69e96fad18d11592fa2965f11d0b556b159b0
mehdi <mehdi.grant@gmail.com>
2022-08-26 23:25:38 +0430

    adding download url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import AnalogClock from "./components/analog-clock/AnalogClock.component";
 import CustomeButton from "./components/CustomeButton/CustomeButton";
-import Footer from './components/Section-Footer/Footer'
+import Footer from "./components/Section-Footer/Footer";
 import { Card } from "./components/card/Card.component";
 
 function App() {
@@ -14,29 +14,30 @@ function App() {
 					You can download the component and inject it directly into your
 					projects.
 				</p>
-				<CustomeButton cName="contained" url="some">
+				<CustomeButton
+					cName="contained"
+					url="https://drive.google.com/file/d/1cmToZnWGK6Oyob6nQP1eBePSfK6mysA5/view?usp=sharing"
+				>
 					Download Component
 				</CustomeButton>
-				<div className="card-overview" >
+				<div className="card-overview">
+					<Card>
+						<h3>Default</h3>
+						<AnalogClock />
+					</Card>
 
-				<Card>
-					<h3>Default</h3>
-					<AnalogClock />
-				</Card>
+					<Card>
+						<h3>Dashed Digits</h3>
+						<AnalogClock dashed />
+					</Card>
+					<Card>
+						<h3>Small Size</h3>
 
-				<Card>
-					<h3>Dashed Digits</h3>
-					<AnalogClock dashed />
-				</Card>
-				<Card>
-				<h3>Small Size</h3>
-
-					<AnalogClock small/>
-				</Card>
+						<AnalogClock small />
+					</Card>
 				</div>
-
 			</header>
-			<Footer/>
+			<Footer />
 		</div>
 	);
 }
